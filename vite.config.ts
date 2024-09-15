df7a0b429a4e35bfe2586a51fa5480d2ee125e1b
somaia96 <somaia96.sh@gmail.com>
2024-09-15 05:07:33 +0300

    refresh problems

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import serve from 'rollup-plugin-serve';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+      react(),
+      reactRefresh(),
+      serve({
+        open: true,
+        contentBase: '',
+        historyApiFallback: true,
+      }),
+    ],
+    server: {
+      middlewareMode: 'html',
+    },
+  }
+)
