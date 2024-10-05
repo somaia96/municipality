310e17f82e3bfb7aab16ae37a4e72303203634f6
somaia96 <somaia96.sh@gmail.com>
2024-10-05 06:51:49 +0300

    fix map

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
             Google map
           </h3>
           <div className="rounded-lg border overflow-hidden">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d6650.034739658197!2d36.31010582658333!3d33.55292502492922!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sar!2sro!4v1726253349605!5m2!1sar!2sro"
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d415.49638608355065!2d36.356415138279495!3d33.58009990823834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1518ef4f59cdd34d%3A0xa6e95d190172968d!2z2LbYp9it2YrYqSDYp9mE2KPYs9iv2Iwg2LPZiNix2YrYpw!5e0!3m2!1sar!2sro!4v1728100154011!5m2!1sar!2sro"
               width="auto" height="auto" style={{ border: 0 }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
